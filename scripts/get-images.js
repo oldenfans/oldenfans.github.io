d7f5af5c8ea43c48436789b5e558c328cd8c8b6a
Silver <sterling.w.white@gmail.com>
2024-10-01 18:33:04 -0500

    Initial commit

---
+++ scripts/get-images.js
@@ -0,0 +1,31 @@
+async function get_images() {
+  const response = await fetch("https://images.search.yahoo.com/search/images;?p=shrek");
+  const html = await response.text();
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, "text/html");
+  const images = doc.querySelectorAll("img");
+  
+  const chosen_images = [];
+  for (let i = 0; i < 25; i++) {
+    chosen_images.push(images[Math.floor(Math.random() * images.length)]);
+  }
+  
+  chosen_images_src = [];
+  chosen_images.forEach(function(img) {
+    chosen_images_src.push(img.src);
+  });
+
+  return chosen_images_src;
+}
+
+setInterval((async () => {
+  const images = await get_images();
+  for (let i = 0; i < images.length; i++) {
+    const img = document.createElement("img");
+    img.src = images[i];
+    img.width = 300;
+    img.loading = "lazy";
+  
+    document.getElementById("picturesdiv").appendChild(img);
+  }
+}), 1000);
